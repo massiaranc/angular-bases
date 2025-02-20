b8a618fa080614fd71ccdfb31145bcc280e2d077
Massiel <marancibia4@gmail.com>
2025-02-20 17:10:07 -0300

    fin de section 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
+
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./components/counter/counter.component";
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports:[
+    CounterComponent,
+  ],
+  exports:[
+    CounterComponent,
+  ]
+})
+export class CounterModule{}
