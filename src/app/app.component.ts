8fcbeb9ec1cf3c981f313be7fb3ba58adf1511fe
MassielSandoval <masandoval@ext.falabella.cl>
2025-03-06 15:15:28 -0300

    proyecto etapa final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public title: string = 'Mi primera paágina';
-  }
+  title = 'Fernando';
+}
